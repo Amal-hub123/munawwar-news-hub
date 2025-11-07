3148571721ca5a5c531e1bcf2f9c99817e101964
Amal-hub123 <alamal0562666215@gmail.com>
2025-11-07 18:15:32 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -87,7 +87,7 @@ const Index = () => {
           <h2 className="text-3xl font-bold mb-8 text-start">أحدث المقالات </h2>
            */}
 
- <section className="py-12 px-4 bg-muted/30">
+ <section className="py-12  bg-muted/30">
       <div className="container mx-auto">
         <h2 className="text-3xl font-bold mb-8 text-start text-primary">أحدث المقالات </h2>
         </div>
@@ -99,7 +99,7 @@ const Index = () => {
               ))}
             </div>
           ) : (
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
               {articles.map((article) => (
                 <ArticleCard
                   key={article.id}
