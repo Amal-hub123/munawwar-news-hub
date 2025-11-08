1ed48ccd700fc78e53faf3813d13acc785d956f2
Amal-hub123 <alamal0562666215@gmail.com>
2025-11-08 09:50:08 +0200

    Update ProductsSlider.tsx

--- src/components/ProductsSlider.tsx
+++ src/components/ProductsSlider.tsx
@@ -43,7 +43,7 @@ const ProductsSlider = () => {
 
   return (
     <section className="py-12 px-4 bg-muted/30">
-      <div className="container mx-auto">
+      <div className="container " style={{paddingRight:'1rem'}}>
         <h2 className="text-3xl font-bold mb-8 text-start text-primary">منتجات المنحنى</h2>
         
         <div className="relative ">
