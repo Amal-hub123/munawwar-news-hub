e0369ba461c3795be37be672bb8320c80441f433
Amal-hub123 <alamal0562666215@gmail.com>
2025-11-10 12:40:39 +0200

    Update ProductDetail.tsx

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -97,7 +97,7 @@ const ProductDetail = () => {
         <h2 className="text-3xl font-bold mb-6 px-8 text-start pt-6">المقالات ذات الصلة</h2>
         
         {articles && articles.length > 0 ? (
-          <div className="grid md:grid-cols-2 gap-6">
+          <div className="grid md:grid-cols-2 gap-6 px-8">
             {articles.map((article) => (
               <ArticleCard
                 key={article.id}
