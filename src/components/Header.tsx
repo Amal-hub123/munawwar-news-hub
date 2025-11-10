0d0feaeac258a4ef6d483ee710d0612be468587f
Amal-hub123 <alamal0562666215@gmail.com>
2025-11-10 10:38:00 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,6 +20,7 @@ import logo from "@/assets/النهائي.png";
 import { supabase } from "@/integrations/supabase/client";
 import { useToast } from "@/hooks/use-toast";
 
+
 export const Header = () => {
   const [open, setOpen] = useState(false);
   const [user, setUser] = useState(null);
