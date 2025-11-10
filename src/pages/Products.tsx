06b5ff76994397b1d5c10fa276133b0b3a042b0e
Amal-hub123 <alamal0562666215@gmail.com>
2025-11-10 09:49:56 +0200

    Update Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -54,8 +54,8 @@ const Products = () => {
                   to={`/products/${product.id}`}
                   className="group"
                 >
-                  <div  className="bg-card rounded-lg overflow-hidden shadow-md hover:shadow-xl transition-all duration-300">
-                    <div className="aspect-square overflow-hidden" style={{borderRadius:'70px !important'}}>
+                  <div style={{borderRadius:'70px !important'}} className="bg-card rounded-lg overflow-hidden shadow-md hover:shadow-xl transition-all duration-300">
+                    <div className="aspect-square overflow-hidden" >
                       <img
                         src={product.image_url}
                         alt={product.name}
