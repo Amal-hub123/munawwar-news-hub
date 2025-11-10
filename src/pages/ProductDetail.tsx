4c2efa826643c922cddc6e62fa239be74287e155
Amal-hub123 <alamal0562666215@gmail.com>
2025-11-10 12:21:59 +0200

    Update ProductDetail.tsx

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -74,7 +74,7 @@ const ProductDetail = () => {
 
       
       <div className="container mx-auto px-8 py-8">
-       <div className="bg-card rounded-lg p-3 mb-3 shadow-md">
+       <div className="bg-card rounded-lg p-3 mb-3 shadow-md" style={{backgroundColor:'#00343a'}}>
   <div className="flex flex-row md:flex-row items-start md:items-center gap-4 md:gap-8">
     {/* الصورة */}
     <img
@@ -87,7 +87,7 @@ const ProductDetail = () => {
     <div className="flex-1 text-right w-full">
       <h1 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4">{product.name}</h1>
       {product.description && (
-        <p className="text-sm md:text-lg text-muted-foreground">{product.description}</p>
+        <p className="text-sm md:text-lg ">{product.description}</p>
       )}
     </div>
   </div>
