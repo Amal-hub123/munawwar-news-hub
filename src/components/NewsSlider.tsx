ce2f3a908e7260350361614e5c009e7c748ca8df
Amal-hub123 <alamal0562666215@gmail.com>
2025-11-11 09:28:43 +0200

    Update NewsSlider.tsx

--- src/components/NewsSlider.tsx
+++ src/components/NewsSlider.tsx
@@ -6,6 +6,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { supabase } from "@/integrations/supabase/client";
 import { useToast } from "@/hooks/use-toast";
 
+
 interface NewsItem {
   id: string;
   title: string;
