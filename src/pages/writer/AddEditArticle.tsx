6afcb6440fad94e7a968b131493539399411a9ff
Amal-hub123 <alamal0562666215@gmail.com>
2025-11-11 09:42:20 +0200

    Update AddEditArticle.tsx

--- src/pages/writer/AddEditArticle.tsx
+++ src/pages/writer/AddEditArticle.tsx
@@ -182,6 +182,7 @@ export const AddEditArticle = () => {
             <div>
               <Label>المنتج </Label>
               <Select
+                style={{    justifyContent: 'right'}}
                 value={formData.product_id || "none"}
                 onValueChange={(value) => setFormData({ ...formData, product_id: value === "none" ? "" : value })}
               >
