74a10ff652cf55e02abeaec5d9ce7021d1e6ceb2
AlmonhnaSA <almonhnaa@gmail.com>
2026-03-08 03:44:04 -0700

    Update index.ts

--- supabase/functions/og-share/index.ts
+++ supabase/functions/og-share/index.ts
@@ -24,7 +24,7 @@ Deno.serve(async (req) => {
   const supabase = createClient(supabaseUrl, supabaseKey);
 
   const table = type === ("article" || "articles") ? "articles" : "news";
-  const pagePath = type === ("article" || "articles") ? `/articles/${id}` : `/news/${id}`;
+  const pagePath =  `/articles/${id}`  //type === ("article" || "articles") ? `/articles/${id}` : `/news/${id}`;
   const redirectUrl = `${SITE_URL}${pagePath}`;
 
   const { data, error } = await supabase
