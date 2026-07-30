d16a64ef094be489e441826e1c29b5e0faec7ad8
AlmonhnaSA <almonhnaa@gmail.com>
2026-07-30 08:16:30 -0700

    Update ArticleDetail.tsx

--- src/pages/ArticleDetail.tsx
+++ src/pages/ArticleDetail.tsx
@@ -248,7 +248,7 @@ const ArticleDetail = () => {
         <div className="prose prose-lg max-w-none">
           <p className="text-xl text-muted-foreground mb-6"  style={{textAlign: "justify"}}>{article.excerpt}</p>
           <div
-            className="site-content [&_*]:!font-sans [&_*]:!text-gray-900 [&_h1]:!text-3xl [&_h2]:!text-2xl [&_h3]:!text-xl [&_*]:!text-[17px] [&_*]:!leading-[1.9]"
+            className="site-content  [&_*]:!text-gray-900 [&_h1]:!text-3xl [&_h2]:!text-2xl [&_h3]:!text-xl [&_*]:!text-[17px] [&_*]:!leading-[1.9]"
             style={{padding:"5px", borderRadius: "20px" , backgroundColor:"#f5f0e1d1"}}
             dangerouslySetInnerHTML={{ __html: article.content }}
           />
