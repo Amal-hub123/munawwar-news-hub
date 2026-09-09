673a7c6fd2531634f06736b78d3390d9d33f2bc3
Amal-hub123 <alamal0562666215@gmail.com>
2026-09-09 00:57:32 -0700

    Update EditorialCurve.tsx

--- src/components/home/EditorialCurve.tsx
+++ src/components/home/EditorialCurve.tsx
@@ -14,7 +14,7 @@ export const EditorialCurve = ({ className, flip = false }: EditorialCurveProps)
   >
     <path
       pathLength="1"
-     d="M-100 150 C200 0 450 20 650 110 C850 200 1100 200 1400 0"
+       d="M-100 150 C200 0 450 20 650 110 C850 200 1100 200 1400 0"
       fill="none"
       stroke="currentColor"
       strokeWidth="1.5"
