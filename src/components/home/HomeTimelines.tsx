ad1a6f593cd9f963ce1fe22471adbec1e66db541
Amal-hub123 <alamal0562666215@gmail.com>
2026-09-15 05:17:36 -0700

    Update HomeTimelines.tsx

--- src/components/home/HomeTimelines.tsx
+++ src/components/home/HomeTimelines.tsx
@@ -31,7 +31,7 @@ const InteractiveTimeline = ({
    */
   const VIEWBOX_HEIGHT = 180;
   const BASE_Y = 90;
-  const AMPLITUDE = 32;
+  const AMPLITUDE = 30;
 
   /*
    * هذه هي المعادلة الوحيدة المستخدمة
